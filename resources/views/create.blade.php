63cc5283ec1e7b917f135d3d8441514146d72e4f
juliano santos <jot.asa@hotmail.com>
2023-05-16 15:01:47 -0300

    correct

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -8,7 +8,13 @@
 </head>
 <body>
     <form action=" {{ route ('supports-store') }} " method="POST">
-       @include('admin.partials.form')
+        @csrf
+        <h1>Nova Dúvida</h1>
+
+        
+        <input type="text" placeholder="Assunto" name="subject" id="subject">
+        <textarea placeholder="Descrição" name="description" id="description" cols="30" rows="10"></textarea>
+        <input type="submit" value="Enviar">
     </form>
 </body>
 </html>
--- resources/views/partials/form.blade.php
+++
@@ -1,7 +0,0 @@
-@csrf
-<h1>Nova Dúvida</h1>
-
-
-<input type="text" placeholder="Assunto" name="subject" id="subject">
-<textarea placeholder="Descrição" name="description" id="description" cols="30" rows="10"></textarea>
-<input type="submit" value="Enviar">
